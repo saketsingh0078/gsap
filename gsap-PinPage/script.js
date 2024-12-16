d60ef532cc96e97851dc106ebab3beb6be6e2bc3
saketsingh0078 <saketkrsingh0078@gmail.com>
2024-12-17 01:21:15 +0530

    completed gsap learning

---
+++ gsap-PinPage/script.js
@@ -0,0 +1,25 @@
+gsap.to("#page2 img", {
+  width: "80%",
+  scrollTrigger: {
+    trigger: "#page2",
+    scroller: "body",
+    markers: true,
+    start: "top 0",
+    end: "top -100%",
+    scrub: 2,
+    pin: true,
+  },
+});
+
+gsap.to("#page4 h1", {
+  transform: "translateX(-125%)",
+  scrollTrigger: {
+    trigger: "#page4",
+    scroller: "body",
+    markers: true,
+    start: "top 0",
+    end: "top -100%",
+    scrub: 2,
+    pin: true,
+  },
+});
